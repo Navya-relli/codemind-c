5aa5f5814157e352071334d37079091d24e1df3d
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-06-30 20:26:06 +0530

    #include<stdio.h> int rev(int num) {     int re=0,r;     while (num>0)     {         r=num%10;         re=(re*10)+r;         num=num/10;     }     return re; } int main() {     int a;     scanf("%d",&a);     int ar=rev(a);     int c=a*a,d=ar*ar;     if (c==rev(d))     {         printf("True");     }     else     {         printf("False");     }     return 0; }

---
+++ Adam_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int rev(int num)
+{
+    int re=0,r;
+    while (num>0)
+    {
+        r=num%10;
+        re=(re*10)+r;
+        num=num/10;
+    }
+    return re;
+}
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int ar=rev(a);
+    int c=a*a,d=ar*ar;
+    if (c==rev(d))
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
