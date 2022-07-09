0af7eed601fc340135926bafef78c731c3c6261f
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-07-09 15:52:40 +0530

    #include<stdio.h> int main() {     int a,b,arr[100],n,i,s=0,s1=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d %d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]>=a&&(arr[i]<=b))         s+=0;         else         {             printf("%d ",arr[i]);             c++;         }     }     if(c==0)     {         printf("-1");     } }

---
+++ elements_not_in_between_A_and_B.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,arr[100],n,i,s=0,s1=0,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d %d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=a&&(arr[i]<=b))
+        s+=0;
+        else
+        {
+            printf("%d ",arr[i]);
+            c++;
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+}
