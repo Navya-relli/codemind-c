97aaa58e8736eb5da5ba9a9adb53d3ceb5655e8d
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-07-18 21:18:13 +0530

    #include<stdio.h> int main() {     int i,n,arr[100],c=0,d=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             if(i%2!=0)             {                 c++;             }             else             {                 d++;                 break;             }         }     }     if(d==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,arr[100],c=0,d=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            if(i%2!=0)
+            {
+                c++;
+            }
+            else
+            {
+                d++;
+                break;
+            }
+        }
+    }   
+    if(d==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
