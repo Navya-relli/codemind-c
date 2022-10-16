dcafb4e5b735733ab1968690555c4e55160cc7a9
Navya <97428614+Navya-relli@users.noreply.github.com>
2022-10-16 16:56:36 +0530

    #include<stdio.h> int main() {     int n,i,count=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             if(i%2==0)             {                 count++;                 break;             }         }     }     if(count==0)     printf("True");     else     printf("False");     return 0; }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,arr[100],c=0,d=0;
+    int n,i,count=0;
     scanf("%d",&n);
+    int arr[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
@@ -11,23 +12,16 @@ int main()
     {
         if(arr[i]%2!=0)
         {
-            if(i%2!=0)
+            if(i%2==0)
             {
-                c++;
-            }
-            else
-            {
-                d++;
+                count++;
                 break;
             }
         }
-    }   
-    if(d==0)
-    {
-        printf("True");
     }
+    if(count==0)
+    printf("True");
     else
-    {
-        printf("False");
-    }
+    printf("False");
+    return 0;
 }
